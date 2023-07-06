cc4bbb706185ccff111196a7b67f318b1801d388
Wafer Li <omyshokami@gmail.com>
2023-07-06 20:35:54 +0800

    fix: compile error due to leaving BASE45 map somewhere

--- encoding/src/jvmMain/kotlin/diglol/encoding/base45.kt
+++ encoding/src/jvmMain/kotlin/diglol/encoding/base45.kt
@@ -2,12 +2,11 @@
 
 package diglol.encoding
 
-import diglol.encoding.internal.BASE45
 import diglol.encoding.internal.commonDecodeBase45
 import diglol.encoding.internal.commonEncodeBase45
 
-actual fun ByteArray.encodeBase45(): ByteArray = commonEncodeBase45(BASE45)
+actual fun ByteArray.encodeBase45(): ByteArray = commonEncodeBase45()
 actual fun ByteArray.decodeBase45(): ByteArray? = commonDecodeBase45()
 
-actual fun ByteArray.encodeBase45ToString(): String = commonEncodeBase45(BASE45).decodeToString()
+actual fun ByteArray.encodeBase45ToString(): String = commonEncodeBase45().decodeToString()
 actual fun String.decodeBase45ToBytes(): ByteArray? = encodeToByteArray().decodeBase45()
